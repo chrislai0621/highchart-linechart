15f60785d9ab1fee693a10ee9ae1de4ffc5be1b5
chrislai0621 <32324347+chrislai0621@users.noreply.github.com>
2020-09-14 20:47:59 +0800

    修改ios取日期方式

--- js/all.js
+++ js/all.js
@@ -4,12 +4,44 @@ const orignalData =[
     createDate:'2020-09-01 16:02:02'
   },
   {
+    register: 5,
+    createDate: '2020-09-02 18:02:02'
+  },
+  {
+    register: 20,
+    createDate: '2020-09-03 18:02:02'
+  },
+  {
+    register: 6,
+    createDate: '2020-09-04 18:02:02'
+  },
+  {
+    register: 30,
+    createDate: '2020-09-05 18:02:02'
+  },
+  {
+    register: 50,
+    createDate: '2020-09-06 18:02:02'
+  },
+  {
     register: 10,
     createDate: '2020-09-10 16:02:02'
   },
   {
     register: 30,
-    createDate: '2020-09-18 16:02:02'
+    createDate: '2020-09-11 16:02:02'
+  },
+  {
+    register: 20,
+    createDate: '2020-09-12 16:02:02'
+  },
+  {
+    register: 100,
+    createDate: '2020-09-13 16:02:02'
+  },
+  {
+    register: 30,
+    createDate: '2020-09-14 16:02:02'
   },
   {
     register: 20,
@@ -31,17 +63,83 @@ const orignalData =[
     register: 35,
     createDate: '2020-09-30 16:02:02'
   },
+  {
+    register: 100,
+    createDate: '2020-10-20 16:02:02'
+  },
+  {
+    register: 35,
+    createDate: '2020-10-21 16:02:02'
+  },
+  {
+    register: 100,
+    createDate: '2020-10-25 16:02:02'
+  },
+  {
+    register: 35,
+    createDate: '2020-10-30 16:02:02'
+  },
+  {
+    register: 20,
+    createDate: '2020-11-01 16:02:02'
+  },
+  {
+    register: 10,
+    createDate: '2020-11-10 16:02:02'
+  },
+  {
+    register: 30,
+    createDate: '2020-11-18 16:02:02'
+  },
+  {
+    register: 20,
+    createDate: '2020-11-19 16:02:02'
+  },
+  {
+    register: 100,
+    createDate: '2020-11-20 16:02:02'
+  },
+  {
+    register: 35,
+    createDate: '2020-11-21 16:02:02'
+  },
+  {
+    register: 100,
+    createDate: '2020-11-25 16:02:02'
+  },
+  {
+    register: 35,
+    createDate: '2020-11-30 16:02:02'
+  },
 ]
 var dataArray =[];//传入的叠数
 var lineColor ='#BF2621';//折线的颜色
 var chartTitle='注册人数';//x单位
 var yName='人';//y单位
+var dateColumName = 'createDate';
+var unitColumName = 'register';
+var DateRangeType=2;//0本日 1本週 2月 3自訂
+var tickInterval = 24 * 3600 * 1000;
+switch (DateRangeType)
+{
+    case 0:
+    case 1:
+      tickInterval = 24 * 3600 * 1000;
+      break;
+    case 2:
+    case 3:
+      tickInterval = 7 * 24 * 3600 * 1000;
+      break;  
+   default :
+      tickInterval = 7 * 24 * 3600 * 1000;
+
+}
 for(let i = 0;i < orignalData.length ;i++)
 {
-  let date = new Date(orignalData[i].createDate);
-  dataArray.push ([Date.UTC(date.getFullYear(), date.getMonth() , date.getDate()), orignalData[i].register]);
+  let date = new Date(orignalData[i][dateColumName].replace(/-/g, "/"));
+  dataArray.push([Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()), orignalData[i][unitColumName]]);
 }
-Highcharts.chart('container', {
+let parameterObj = {
   chart: {
     type: 'line',
     backgroundColor: 'black',
@@ -56,9 +154,10 @@ Highcharts.chart('container', {
   subtitle: {
     text: '2020/09/01-2020/09/30'
   },
+
   xAxis: {
-    tickInterval: 7 * 24 * 3600 * 1000, // one week
-    tickColor:'#C6AB80',
+    tickInterval:tickInterval,
+    tickColor: '#C6AB80',
     type: 'datetime',
     dateTimeLabelFormats: { // don't display the dummy year
       month: '%b %e',
@@ -68,7 +167,7 @@ Highcharts.chart('container', {
       text: '',
       style: {
         color: '#C6AB80'
-      }     
+      }
     },
     lineColor: '#C6AB80', //基線顏色
     gridLineColor: '#C6AB80',//網格（豎線）顏色
@@ -77,10 +176,10 @@ Highcharts.chart('container', {
         color: '#C6AB80' //X軸 123456的顏色
       },
       formatter: function () {
-        const  date = new Date(this.value);
-        const m = date.getMonth() +1;
-        
-        return m+'/' + date.getDate();
+        const date = new Date(this.value);
+        const m = date.getMonth() + 1;
+
+        return m + '/' + date.getDate();
       },
     },
   },
@@ -89,7 +188,7 @@ Highcharts.chart('container', {
       text: '',
       style: {
         color: '#C6AB80'
-      } 
+      }
     },
     lineColor: '#C6AB80',
     labels: {
@@ -102,16 +201,16 @@ Highcharts.chart('container', {
   tooltip: {
     formatter: function () {
       const date = new Date(this.x);
-       const m = date.getMonth() +1;
-      let htmlStr =`<b>${m}月${date.getDate()}日</b><br>
-     ${chartTitle}: ${this.y}${yName}`;        
-        return htmlStr;
-      },
+      const m = date.getMonth() + 1;
+      let htmlStr = `<b>${m}月${date.getDate()}日</b><br>
+     ${chartTitle}: ${this.y}${yName}`;
+      return htmlStr;
+    },
   },
   plotOptions: {
     series: {
       marker: {
-        enabled: true 
+        enabled: true
       },
       color: this.lineColor, // 中間折線圖的顏色條改
       showInLegend: false //隱藏系列名稱
@@ -120,11 +219,11 @@ Highcharts.chart('container', {
   credits: {
     enabled: false//不显示右下角官方LOGO
   },
-  exporting : //匯出設定
+  exporting: //匯出設定
   {
     enabled: false //是否顯示匯出的漢堡選單
   },
-  
+
   series: [{
     name: "",
     data: this.dataArray,
@@ -146,4 +245,5 @@ Highcharts.chart('container', {
       }
     }]
   }
-});
+}
+Highcharts.chart('container', parameterObj);
